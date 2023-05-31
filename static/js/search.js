944d903b7216b47fb4c95f880bc977c4b8400782
sandbox co.,ltd <info@sndbox.jp>
2023-05-31 11:59:52 +0900

    algolia

--- static/js/search.js
+++ static/js/search.js
@@ -3,21 +3,20 @@
 function extractTextWithEmTag(text) {
   const startIndex = text.indexOf('<em>');
   const endIndex = text.indexOf('</em>');
-
   if (startIndex !== -1 && endIndex !== -1) {
-    const start = Math.max(0, startIndex - 0);
-    const end = Math.min(text.length, endIndex + 30);
+    const start = Math.max(0, startIndex - 10);
+    const end = Math.min(text.length, endIndex + 100);
     const extractedText = text.substring(start, end);
-    return extractedText;
+    return cutStr(extractedText, 50);
   }
-  return cutStr(text)
+  return cutStr(text, 50)
 }
-function cutStr (text) {
-  if (text.length <= 30) {
+function cutStr (text, len = 30) {
+  if (text.length <= len) {
     return text;
   }
 
-  const truncatedText = text.substring(0, 30) + '...';
+  const truncatedText = text.substring(0, len) + '...';
   return truncatedText;
 }
 
@@ -36,23 +35,20 @@ createApp({
 
     // 入力監視の開始
     let textboxDom = document.getElementById('nipoSBox')
-    console.log('DOMは', textboxDom)
     textboxDom.addEventListener('input', function() {
       clearTimeout(timeoutId); // 前回の遅延実行をキャンセル
       timeoutId = setTimeout(function() {
         mySearch(); // 遅延後に実行
-      }, 100);
+      }, 500);
     });
   
     // 検索関数
     function mySearch() {
       resultArr.value = []
       // 実行したい処理を記述する
-      console.log('検索処理を実行します');
       const text = textboxDom.value;
       index.search(text).then(( { hits }) => {
         hits.forEach(function(hit) {
-          console.log(hit);
           const shortTitle = cutStr(hit.title)
           const hilights = hit._highlightResult.summary.value
           const shortSummary = extractTextWithEmTag(hilights)
@@ -61,7 +57,6 @@ createApp({
       }).catch((err) => {
         console.error('エラーが発生しました:', err);
       });
-  
     }
   
     return {

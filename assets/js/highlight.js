5625a0a5264f24ba73deea3a2555687a059753d1
sandbox co.,ltd <info@sndbox.jp>
2023-07-09 23:21:47 +0900

    ｌ

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -5,21 +5,21 @@ import typescript from 'highlight.js/lib/languages/typescript'
 import json from 'highlight.js/lib/languages/json';
 import bash from 'highlight.js/lib/languages/bash';
 import xml from 'highlight.js/lib/languages/xml';
-import ini from 'highlight.js/lib/languages/ini';
-import yaml from 'highlight.js/lib/languages/yaml';
-import markdown from 'highlight.js/lib/languages/markdown';
-import python from 'highlight.js/lib/languages/python';
+// import ini from 'highlight.js/lib/languages/ini';
+// import yaml from 'highlight.js/lib/languages/yaml';
+// import markdown from 'highlight.js/lib/languages/markdown';
+// import python from 'highlight.js/lib/languages/python';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('json', json);
 hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('html', xml);
-hljs.registerLanguage('ini', ini);
-hljs.registerLanguage('toml', ini);
-hljs.registerLanguage('yaml', yaml);
-hljs.registerLanguage('md', markdown);
-hljs.registerLanguage('python', python);
+// hljs.registerLanguage('ini', ini);
+// hljs.registerLanguage('toml', ini);
+// hljs.registerLanguage('yaml', yaml);
+// hljs.registerLanguage('md', markdown);
+// hljs.registerLanguage('python', python);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
